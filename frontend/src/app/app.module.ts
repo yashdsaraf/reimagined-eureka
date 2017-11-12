6f9ad4bbfbdc102a1e08baed39159519821bd1b1
Yash D. Saraf <yashdsaraf@gmail.com>
2017-11-12 19:57:36 +0530

    Add and configure `angular-jwt` dependency

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -17,8 +17,10 @@
 import {NgModule} from '@angular/core'
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import {BrowserModule} from '@angular/platform-browser'
+import {HttpClientModule} from '@angular/common/http'
 
 import {CodemirrorModule} from 'ng2-codemirror'
+import {JwtModule} from '@auth0/angular-jwt'
 import {SuiModule} from 'ng2-semantic-ui'
 
 import {AppComponent} from './app.component'
@@ -49,6 +51,15 @@ import {ToolbarComponent} from './components/toolbar/toolbar.component'
     BrowserAnimationsModule,
     BrowserModule,
     CodemirrorModule,
+    HttpClientModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: () => {
+          return localStorage.getItem('access_token')
+        },
+        whitelistedDomains: ['localhost:8181']
+      }
+    }),
     SuiModule
   ],
   providers: [],
