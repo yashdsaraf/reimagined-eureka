8a74f40a97a2f8ec002e6797bf050b793df8d9c7
Yash D. Saraf <yashdsaraf@gmail.com>
2017-11-12 19:26:43 +0530

    Add security and auth server configs

--- server/src/main/java/org/tyit/pnc/config/AuthorizationServerConfig.java
+++ server/src/main/java/org/tyit/pnc/config/AuthorizationServerConfig.java
@@ -15,10 +15,20 @@
  */
 package org.tyit.pnc.config;
 
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 /**
  *
@@ -28,5 +38,58 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableAuthorizationServer
 @EnableResourceServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+  @Value("${security.jwt.client-id}")
+  private String clientId;
+
+  @Value("${security.jwt.grant-types}")
+  private String grantTypes;
+
+  @Value("${security.jwt.scopes}")
+  private String scopes;
+
+  @Value("${security.jwt.resource-id}")
+  private String resourceId;
+  
+  @Value("${security.jwt.refresh-token-validity}")
+  private Integer refreshTokenValidity;
+  
+  @Value("${security.jwt.access-token-validity}")
+  private Integer accessTokenValidity;
+
+  @Autowired
+  private UserDetailsService userDetailsService;
+  
+  @Autowired
+  private TokenStore tokenStore;
+
+  @Autowired
+  private JwtAccessTokenConverter accessTokenConverter;
+
+  @Autowired
+  private AuthenticationManager authenticationManager;
+
+  @Override
+  public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+    clients
+            .inMemory()
+            .withClient(clientId)
+            .authorizedGrantTypes(grantTypes.split(","))
+            .scopes(scopes.split(","))
+            .accessTokenValiditySeconds(accessTokenValidity)
+            .refreshTokenValiditySeconds(refreshTokenValidity)
+            .resourceIds(resourceId);
+  }
+
+  @Override
+  public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+    TokenEnhancerChain enhancerChain = new TokenEnhancerChain();
+    enhancerChain.setTokenEnhancers(Arrays.asList(accessTokenConverter));
+    endpoints.tokenStore(tokenStore)
+            .userDetailsService(userDetailsService)
+            .accessTokenConverter(accessTokenConverter)
+            .tokenEnhancer(enhancerChain)
+            .authenticationManager(authenticationManager);
+  }
   
 }
--- server/src/main/java/org/tyit/pnc/config/SecurityConfig.java
+++ server/src/main/java/org/tyit/pnc/config/SecurityConfig.java
@@ -15,10 +15,24 @@
  */
 package org.tyit.pnc.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 /**
  *
@@ -28,5 +42,57 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-  
+
+  @Value("${security.encoding-strength}")
+  private Integer encodingStrength;
+
+  @Value("${security.realm}")
+  private String realm;
+
+  @Value("${security.signing-key}")
+  private String signingKey;
+
+  @Autowired
+  private UserDetailsService userDetailsService;
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService)
+            .passwordEncoder(new ShaPasswordEncoder(encodingStrength));
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+            .cors().and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and().csrf().disable()
+            .authorizeRequests()
+            .anyRequest().authenticated()
+            .and().httpBasic()
+            .realmName(realm);
+  }
+
+  @Bean
+  public JwtAccessTokenConverter accessTokenConverter() {
+    JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+    converter.setSigningKey(signingKey);
+    return converter;
+  }
+
+  @Bean
+  public TokenStore tokenStore() {
+    return new JwtTokenStore(accessTokenConverter());
+  }
+
+  @Bean
+  @Primary
+  public DefaultTokenServices tokenServices() {
+    DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+    defaultTokenServices.setTokenStore(tokenStore());
+    defaultTokenServices.setSupportRefreshToken(true);
+    return defaultTokenServices;
+  }
+
 }
