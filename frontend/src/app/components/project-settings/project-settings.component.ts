83576975a86ceed5c9e281d37a4aa796265bbc3e
Yash D. Saraf <yashdsaraf@gmail.com>
2018-02-10 20:26:30 +0530

    Add all the modes and themes for codemirror

--- frontend/src/app/components/project-settings/project-settings.component.ts
+++ frontend/src/app/components/project-settings/project-settings.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {isMobile} from '../../app.component'
+import {Themes} from '../../utils/themes';
 
-declare var $:any
+declare const $: any
 
 @Component({
   selector: 'app-project-settings',
@@ -11,16 +12,19 @@ declare var $:any
 export class ProjectSettingsComponent implements OnInit {
 
   isMobile: boolean
+  selectedTheme = 'github'
+  themes: string[]
 
   constructor() {
     this.isMobile = isMobile
+    this.themes = Themes
+    console.log(Themes)
   }
 
   ngOnInit() {
   }
 
   ngAfterViewInit(){
-
     $('.fullscreen.modal').modal('attach events', '#edit-run', 'show')
   }
 
