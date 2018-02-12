2a305428c47f69a6dfbea1b51115e88a4618a0a1
Yash D. Saraf <yashdsaraf@gmail.com>
2018-02-12 14:18:40 +0530

    Add license header to project settings ts

--- frontend/src/app/components/project-settings/project-settings.component.ts
+++ frontend/src/app/components/project-settings/project-settings.component.ts
@@ -1,25 +1,36 @@
-import {
-  Component,
-  OnDestroy
-} from '@angular/core'
+/*
+ * Copyright 2017 Yash D. Saraf, Raees R. Mulla and Sachin S. Negi.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Component} from '@angular/core'
 
 import {isMobile} from '../../app.component'
 import {Themes} from '../../utils/themes'
 
 import {EditorConfigService} from '../../services/editor-config.service'
-import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-project-settings',
   templateUrl: './project-settings.component.html',
   styleUrls: ['./project-settings.component.sass']
 })
-export class ProjectSettingsComponent implements OnDestroy {
+export class ProjectSettingsComponent {
 
   isMobile: boolean
   themes: string[]
   selectedTheme: string
-  editorConfigSubscription: Subscription
   showModal = false
 
   constructor(private editorConfigService: EditorConfigService) {
@@ -30,10 +41,6 @@ export class ProjectSettingsComponent implements OnDestroy {
 
   ngAfterViewInit() {
 
-  }
-
-  ngOnDestroy() {
-    this.editorConfigSubscription.unsubscribe()
   }
 
   onClick(theme: string) {
