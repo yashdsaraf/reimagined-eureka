fc0d0064cb4fd58dd1b4b5ea7691c12c96b7f816
Yash D. Saraf <yashdsaraf@gmail.com>
2018-02-13 23:06:59 +0530

    Add a controller for run commands and entrypoint

---
+++ server/src/main/java/org/tyit/pnc/service/ProjectConfigService.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2018 Yash D. Saraf, Raees R. Mulla and Sachin S. Negi.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tyit.pnc.service;
+
+import org.tyit.pnc.model.Docker;
+
+/**
+ *
+ * @author Yash D. Saraf <yashdsaraf@gmail.com>
+ */
+public class ProjectConfigService {
+
+  public void getRunCommands(Docker docker) {
+
+  }
+
+  public void setRunCommands(Docker docker) {
+
+  }
+
+  public void getEntrypoint(Docker docker) {
+
+  }
+
+  public void setEntrypoint(Docker docker) {
+
+  }
+
+}
